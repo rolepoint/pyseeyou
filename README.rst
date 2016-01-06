76f50a443dc6a8ff2e2eddffe4b29a14e8ed622e
Siame Rafiq <siame@rolepoint.com>
2016-01-06 19:50:16 +0000

    Working grammar, README and requirements

---
+++ README.rst
@@ -0,0 +1,9 @@
+pyseeyou
+========
+
+A Python ICU parsing tool,
+using `parsimonious <https://github.com/erikrose/parsimonious>`_ to parse a
+PEG, which has been taken from
+`messageformat.js <https://github.com/SlexAxton/messageformat.js>`_.
+
+Written for Python 2.7
---
+++ pyseeyou/grammar.py
@@ -0,0 +1,32 @@
+from __future__ import unicode_literals
+
+from parsimonious.grammar import Grammar
+
+g = Grammar('''
+            message_format_pattern =
+                (message_format_element/octothorpe/string)*
+
+            message_format_element = "{" _ id ("," element_format)? _ "}"
+
+            element_format = (_ "plural" _ "," _ plural_format_pattern _) /
+                (_ "selectordinal" _ "," _ plural_format_pattern _) /
+                (_ "select" _ "," _ select_format_pattern _) /
+                (_ id arg_style_pattern*)
+
+            plural_format_pattern = offset? (plural_form)+
+            plural_form = _ plural_key _ "{" _ message_format_pattern _ "}"
+            plural_key = id / ("=" digits)
+
+            select_format_pattern = select_form+
+            select_form = _ id _ "{" _ message_format_pattern _ "}"
+
+            arg_style_pattern = _ "," _ id _
+
+            offset = _ "offset" _ ":" _ digits _
+
+            octothorpe = ~"#"
+            string     = (id/~"[^{}]+"/_)+
+            id         = ~"\w+"i
+            digits     = ~"[0-9]+"
+            _          = ~"\s*"
+            ''')
