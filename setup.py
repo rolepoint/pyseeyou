4878199c3ed30fb798390b4f2cae98f94d859e70
Siame Rafiq <siame@rolepoint.com>
2016-01-14 14:48:01 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyseeyou',
+    version='0.1.0',
+    description='A Python Parser for the ICU MessageFormat.',
+    author='Siame Rafiq',
+    author_email='mail@sia.me.uk',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['parsimonious', 'toolz'],
+    tests_require=['pytest']
+)
