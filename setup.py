cb9c1b42a11704e121fb849603668abdc110e08f
Siame Rafiq <siame@rolepoint.com>
2016-07-11 13:45:07 +0100

    Up version number to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyseeyou',
-    version='0.1.0',
+    version='0.2.0',
     description='A Python Parser for the ICU MessageFormat.',
     author='Siame Rafiq',
     author_email='mail@sia.me.uk',
