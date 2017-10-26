b7cc6302904e7adbc21edf65a797ded421b35d9a
Siame Rafiq <mail@sia.me.uk>
2017-10-26 17:51:01 +0100

    Add future as required lib for pypi install

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Siame Rafiq',
     author_email='mail@sia.me.uk',
     packages=find_packages(exclude=['tests']),
-    install_requires=['parsimonious', 'toolz'],
+    install_requires=['parsimonious', 'toolz', 'future'],
     tests_require=['pytest'],
     url='https://github.com/rolepoint/pyseeyou',
     download_url='https://github.com/rolepoint/pyseeyou/archive/v0.3.0.tar.gz',
