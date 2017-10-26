72739d2d1701897a2453f5bcd1f1faf24bd12a67
Siame Rafiq <mail@sia.me.uk>
2017-10-26 17:06:21 +0100

    Release v0.3.0

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyseeyou',
-    version='0.2.0',
+    version='0.3.0',
     description='A Python Parser for the ICU MessageFormat.',
     author='Siame Rafiq',
     author_email='mail@sia.me.uk',
     packages=find_packages(exclude=['tests']),
     install_requires=['parsimonious', 'toolz'],
-    tests_require=['pytest']
+    tests_require=['pytest'],
+    url='https://github.com/rolepoint/pyseeyou',
+    download_url='https://github.com/rolepoint/pyseeyou/archive/v0.3.0.tar.gz',
+    keywords='python i18n messageformat python3 python2'
 )
