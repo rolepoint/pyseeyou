424ead2d2e686fc22323a7c73d19af3846dc6f5e
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2019-03-07 17:23:02 +0700

    Add lookup_closest_locale test

--- test/test_locales.py
+++ test/test_locales.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pyseeyou.locales import get_parts_of_num
+from pyseeyou.locales import get_parts_of_num, lookup_closest_locale
 
 def test_get_parts_of_num():
     assert get_parts_of_num('1') == (1, 1, 0, 0, 0, 0)
@@ -10,3 +10,10 @@ def test_get_parts_of_num():
     assert get_parts_of_num('1.30') == (1.30, 1, 2, 1, 30, 3)
     assert get_parts_of_num('1.03') == (1.03, 1, 2, 2, 3, 3)
     assert get_parts_of_num('1.230') == (1.230, 1, 3, 2, 230, 23)
+
+def test_lookup_closest_locale():
+    dummy_dict = { 'uk': 'Ukrainian', 'fr': 'French' }
+
+    assert lookup_closest_locale('fr_FR', dummy_dict) == 'fr'
+    assert not lookup_closest_locale('be-BY', dummy_dict)
+
